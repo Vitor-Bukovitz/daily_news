771705fd74128fa4439b1c74871b007fa14b8e23
Vitor Bukovitz <vitorbukovitz@hotmail.com>
2022-01-22 21:47:20 -0300

    feat: added home domain module

--- lib/features/home/domain/home_domain_module.dart
+++ lib/features/home/domain/home_domain_module.dart
@@ -2,9 +2,9 @@ import 'package:daily_news/features/home/data/home_data_module.dart';
 import 'package:daily_news/features/home/domain/usecases/get_articles_usecase.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// final getArticlesUsecaseProvider = FutureProvider<GetArticlesUsecase>(
-//   (ref) async {
-//     final repository = await ref.watch(articlesRepositoryProvider.future);
-//     return GetArticlesUsecase(repository);
-//   },
-// );
+final getArticlesUsecaseProvider = Provider<GetArticlesUsecase>(
+  (ref) {
+    final repository = ref.watch(articlesRepositoryProvider);
+    return GetArticlesUsecase(repository);
+  },
+);
